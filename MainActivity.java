d7fe0d5379fa423c6835fbd2375a78602638d0d3
Vijaybabu-937 <144809777+Vijaybabu-937@users.noreply.github.com>
2023-11-02 19:19:58 +0530

    Quiz_app
       I have used Android Studio Software for designing applications. I have created an app using Java in Android Studio. I have uploaded both Java and Xml files. This is a Quiz application. It is a simple quiz app. Actually, I have designed simple quiz app but for some more updating in app, I have taken some help from the tutorials for final Score of the quiz. I have used another class for questions and options. Instead of finding questions in Main Activity file for changing the questions, I have used question.java file for changing the questions. It was easy to everyone for changing the questions for different quizzes. The final Output of this app gives an Alert dialog box, that shows Score of the quiz and restart the quiz option also.

--- MainActivity.java
+++ MainActivity.java
@@ -1,54 +1,106 @@
-package com.example.mytemp;
+package com.example.myquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    EditText E;
-    TextView T;
-    Button b1,b2;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    @SuppressLint("MissingInflatedId")
+    TextView Totalquestions;
+    TextView question;
+    Button ansA,ansB,ansC,ansD;
+    Button submitbtn;
+
+    int score=0;
+    int totalQuestion = QuestionAnswer.question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer = "";
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        E = findViewById(R.id.editTextNumberDecimal);
-        T = findViewById(R.id.textView2);
-        b1 = findViewById(R.id.button);
-        b2 = findViewById(R.id.button2);
+
+        Totalquestions = findViewById(R.id.TotalQuestions);
+        question = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_A);
+        ansB = findViewById(R.id.ans_B);
+        ansC = findViewById(R.id.ans_C);
+        ansD = findViewById(R.id.ans_D);
+        submitbtn = findViewById(R.id.submit);
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        submitbtn.setOnClickListener(this);
+
+        Totalquestions.setText("Total questions : "+totalQuestion);
+        loadNewQuestion();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        ansA.setBackgroundColor(Color.WHITE);
+        ansB.setBackgroundColor(Color.WHITE);
+        ansC.setBackgroundColor(Color.WHITE);
+        ansD.setBackgroundColor(Color.WHITE);
 
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                double temp = Double.parseDouble(E.getText().toString());
-                double fc;
-                 fc = 0.555*(temp-32);
-                T.setText(String.valueOf(fc));
+        Button clickedButton = (Button) view;
+        if(clickedButton.getId()==R.id.submit){
+            if(selectedAnswer.equals(QuestionAnswer.correctAnswers[currentQuestionIndex])){
+                score++;
             }
-        });
-       b2.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               double temp = Double.parseDouble(E.getText().toString());
-               double cf;
-               cf = (1.8*temp) + 32;
-               T.setText(String.valueOf(cf));
-           }
-       });
+            currentQuestionIndex++;
+            loadNewQuestion();
 
-
-        if(!(E.getText().toString().equals("")));
-        {
-            Toast.makeText(MainActivity.this, "Please enter Temp", Toast.LENGTH_LONG).show();
+        }else{
+            selectedAnswer = clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(Color.MAGENTA);
         }
+
+    }
+    void loadNewQuestion(){
+        if(currentQuestionIndex == totalQuestion){
+            finishQuiz();
+            return;
+        }
+
+        question.setText(QuestionAnswer.question[currentQuestionIndex]);
+        ansA.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
+        ansB.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
+        ansC.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
+        ansD.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
+    }
+
+    void finishQuiz(){
+        String passStatus = " ";
+        if(score > totalQuestion * 0.60){
+            passStatus = "Passed";
+        }else{
+            passStatus = "Failed";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Score is "+ score +"out of" + totalQuestion)
+                .setPositiveButton("Restart",(dialogInterface, i) -> restartQuiz() )
+                .setCancelable(false)
+                .show();
+    }
+
+    void restartQuiz(){
+        score = 0;
+        currentQuestionIndex = 0;
+    loadNewQuestion();
     }
 }
